edf584336ba2deb40d7cec4c86de75bfea8ec2e5
Gamover <gamover@outlook.com>
2020-02-23 23:19:52 +0500

    Channels test

--- Test/Program.cs
+++ Test/Program.cs
@@ -28,9 +28,68 @@ namespace Test
             //Utf8JsonReader
             //JsonSerializer 
             //await RunDefault();
+            await ChannelTest();
+            //Task.WaitAll(Task.Run(StartConsumer),
+            //             Task.Run(StartPublisher));
+        }
+        public static async Task ChannelTest()
+        {
+            var address = Dns.GetHostAddresses("centos0.mshome.net")[0];
+            RabbitMQConnectionBuilder builder = new RabbitMQConnectionBuilder(new IPEndPoint(address, 5672));
+            var connection = builder.ConnectionInfo("gamover", "gam2106", "/")
+                                    .Heartbeat(60 * 10)
+                                    .ProductName("AMQP.Client.RabbitMQ")
+                                    .ProductVersion("0.0.1")
+                                    .ConnectionName("AMQP.Client.RabbitMQ:Test")
+                                    .ClientInformation("TEST TEST TEST")
+                                    .ClientCopyright("©")
+                                    .Build();
+            await connection.StartAsync();
+            var channel1 = await connection.CreateChannel();
+            var channel2 = await connection.CreateChannel();
+            await channel1.ExchangeDeclareAsync("TestExchange", ExchangeType.Direct, arguments: new Dictionary<string, object> { { "TEST_ARGUMENT", true } });
+            var queueOk = await channel1.QueueDeclareAsync("TestQueue", false, false, false, new Dictionary<string, object> { { "TEST_ARGUMENT", true } });
+            await channel1.QueueBindAsync("TestQueue", "TestExchange");
+
+            var consumer1 = await channel1.CreateConsumer("TestQueue", "TestConsumer", noAck: true);
+            consumer1.Received += async (deliver, result) =>
+            {
+                //await deliver.Ack();
+            };
+            var consumer2 = await channel2.CreateConsumer("TestQueue", "TestConsumer", noAck: true);
+            consumer2.Received += async (deliver, result) =>
+            {
+                //await deliver.Ack();
+            };
+
+
+
             
-            Task.WaitAll(Task.Run(StartConsumer),
-                         Task.Run(StartPublisher));
+            
+            var firtsTask = Task.Run(async () =>
+            {
+                var publisher = channel1.CreatePublisher();
+                ContentHeaderProperties properties = new ContentHeaderProperties();
+                properties.AppId = "testapp";
+                while (true)
+                {
+                    await publisher.Publish("TestExchange", string.Empty, false, false, properties, new byte[128*1024]);
+                }
+            });
+            var secondTask = Task.Run(async () =>
+            {
+                var publisher = channel2.CreatePublisher();
+                ContentHeaderProperties properties = new ContentHeaderProperties();
+                properties.AppId = "testapp";
+                while (true)
+                {
+                    await publisher.Publish("TestExchange", string.Empty, false, false, properties, new byte[128*1024]);
+                }
+            });
+
+
+
+            await connection.WaitEndReading();
         }
         private static async Task RunDefault()
         {
